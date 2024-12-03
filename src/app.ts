13b70bdc3c6925204b85eb626aa955cbb839ed00
Jurami Pereira Junior <jurami.junior@gmail.com>
2024-12-03 18:59:39 -0300

    Arrumando a porta da Aplicação

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("ok");
