a536b6a44db00608bf9c4c23039ab5ed93f523f8
Jurami Pereira Junior <jurami.junior@gmail.com>
2024-12-04 23:54:49 -0300

    ajuste endereço

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioClient = new Twilio(accountSid, authToken);
 // Configuração do Google Auth com Dialogflow
 const auth = new GoogleAuth({
-  keyFile: "/opt/render/project/src/src/services/credenciais.json", // Caminho absoluto correto
+  keyFile: "./src/services/credenciais.json", // Caminho relativo correto
   scopes: ["https://www.googleapis.com/auth/dialogflow"],
 });
 
